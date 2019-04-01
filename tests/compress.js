73134b74ac2a9eaedb1674a336e4b6367d0098c9
practicas-ging <practicas-ging@mail.com>
2019-04-01 09:31:54 +0200

    Updated 05_quiz_socket

--- tests/compress.js
+++ tests/compress.js
@@ -1,8 +1,8 @@
 const fs = require('fs-extra');
 const path = require('path');
 const archiver = require('archiver');
+
 const orig = path.resolve(path.join(__dirname, "CORE19-05_quiz_socket.zip"));
-console.log(__dirname);
 const dest = path.resolve(path.join(__dirname ,"../CORE19-05_quiz_socket.zip"));
 const output = fs.createWriteStream(orig);
 const archive = archiver('zip', {
